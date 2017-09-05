2940f1bff50bfbe43f6101ea8e29f589f6d6f68a
kariminus <karim.meciel@gmail.com>
2017-09-05 14:03:53 +0200

    recuperation des donnees de l'api

--- web/assets/js/App.vue
+++ web/assets/js/App.vue
@@ -1,9 +1,28 @@
 <template>
-    <div class="container">
-        <p>Hello from Vue</p>
+    <div>
+        <ul>
+            <li v-for="phone in phones">
+                <p>{{ phone.name }}</p>
+                <p>{{ phone.brand.name }}</p>
+                <img :src="'http://127.0.0.1:8001/uploads/images/phones/' + phone.image">
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-
+    export default {
+        data() {
+            return {
+                phones: [],
+            };
+        },
+        created() {
+            // GET /someUrl
+            this.$http.get('http://127.0.0.1:8001/api/phones').then(response => {
+                this.phones = response.data[0].items;
+                console.log(this.phones);
+            })
+        }
+    }
 </script>
